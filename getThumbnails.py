b019368720dd9c4aebe4b9ae522e817bb956aeb6
Bhargav Srinivasan <bhargav461995@gmail.com>
2018-03-08 16:06:18 -0800

    Run image through google cloud vision API

--- getThumbnails.py
+++ getThumbnails.py
@@ -1,5 +1,14 @@
 import requests
 import csv
+import json
+
+likelihood = {"UNKNOWN": 0,
+              "VERY_UNLIKELY": 0,
+              "UNLIKELY": 2,
+              "POSSIBLE": 10,
+              "LIKELY": 20,
+              "VERY_LIKELY": 100
+              }
 
 payload = {"type": "video",
            "channelId": "UCCqEeDAUf4Mg0GgEN658tkA",
@@ -19,5 +28,41 @@ with open("results.csv", "wb") as csvfile:
         res = r.json()
 
         for i in res['items']:
-            print i['snippet']['title'], i['snippet']['thumbnails']['high']['url']
+            title = i['snippet']['title']
+            thumbnail_url = i['snippet']['thumbnails']['high']['url']
+
+            post_json_body = {
+                "requests": [
+                    {
+                        "image": {
+                            "source": {
+                                "imageUri":
+                                    thumbnail_url
+                            }
+                        },
+                        "features": [
+                            {
+                                "type": "FACE_DETECTION",
+                                "maxResults": 1
+                            }
+                        ]
+                    }
+                ]
+            }
+
+            post_json_body = json.dumps(post_json_body)
+
+            face_response = requests.post(
+                'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBxqrzSWhAdQxI6m_pk_7XTcSzxYctECDs',
+                data=post_json_body)
+
+            try:
+                annotations = face_response.json()['responses'][0]['faceAnnotations'][0]
+
+                joy = likelihood[annotations['joyLikelihood']]
+
+                print title, joy
+            except Exception:
+                pass
+
             writer.writerow([i['snippet']['title'], i['snippet']['thumbnails']['high']['url']])
