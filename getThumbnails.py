ac7ee73308dca7277ddaeb2ba3e42fa2ae1d44e3
Bhargav Srinivasan <bhargav461995@gmail.com>
2018-09-21 21:31:20 -0700

    Get all scores

--- getThumbnails.py
+++ getThumbnails.py
@@ -1,6 +1,7 @@
 import requests
 import csv
 import json
+import time
 import config
 
 face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
@@ -23,7 +24,7 @@ payload = {"type": "video",
            "maxResults": 50,
            "part": "snippet",
            "key": config.youtubeAPI,
-           "pageToken": ""
+           "pageToken": "CLYHEAA"
            }
 
 post_json_body = {
@@ -44,38 +45,76 @@ post_json_body = {
     ]
 }
 
-with open("results.csv", "wb") as csvfile:
+with open("results4.csv", "wb") as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(
+        ["title", "image_url", "sadness", "neutral", "contempt", "disgust", "anger", "surprise", "fear", "happiness"])
 
-    r = requests.get('https://www.googleapis.com/youtube/v3/search', params=payload)
+    for i in range(20):
 
-    if r.status_code == 200:
-        res = r.json()
+        r = requests.get('https://www.googleapis.com/youtube/v3/search', params=payload)
 
-        for i in res['items']:
-            title = i['snippet']['title']
-            thumbnail_url = i['snippet']['thumbnails']['high']['url']
+        if r.status_code == 200:
+            res = r.json()
 
-            # print post_json_body['requests'][0]
+            print res
 
-            post_json_body['requests'][0]['image']['source']['imageUri'] = thumbnail_url
+            if "nextPageToken" in res:
+                payload['pageToken'] = res['nextPageToken']
+            else:
+                break
 
-            # print post_json_body['requests'][0]
+            for i in res['items']:
 
-            response = requests.post(face_api_url, params=params, headers=headers, json={"url": thumbnail_url})
-            faces = response.json()
+                flag = False
 
-            try:
-                annotations = faces[0]['faceAttributes']['emotion']
+                while flag != True:
+                    title = i['snippet']['title']
 
-                print title, max(annotations, key=annotations.get), max(annotations.values())
-                # raw_input()
-                writer.writerow([i['snippet']['title'], i['snippet']['thumbnails']['high']['url']])
-            except (IndexError,UnicodeEncodeError) as e:
-                print i['snippet']['title'], str(e)
-                pass
-            except KeyError:
-                print i['snippet']['title']
-                print faces
-                raw_input()
+                    if "album review" not in title.lower():
+                        flag = True
+                        continue
 
+                    thumbnail_url = i['snippet']['thumbnails']['high']['url']
+
+                    # print post_json_body['requests'][0]
+
+                    post_json_body['requests'][0]['image']['source']['imageUri'] = thumbnail_url
+
+                    # print post_json_body['requests'][0]
+
+                    response = requests.post(face_api_url, params=params, headers=headers, json={"url": thumbnail_url})
+                    faces = response.json()
+
+                    try:
+                        annotations = faces[0]['faceAttributes']['emotion']
+
+                        annotations['title'] = i['snippet']['title']
+                        annotations['image_url'] = i['snippet']['thumbnails']['high']['url']
+
+                        print annotations
+
+                        writer.writerow([
+                            annotations['title'],
+                            annotations['image_url'],
+                            annotations['sadness'],
+                            annotations['neutral'],
+                            annotations['contempt'],
+                            annotations['disgust'],
+                            annotations['anger'],
+                            annotations['surprise'],
+                            annotations['fear'],
+                            annotations['happiness'],
+                        ])
+                        flag = True
+                    except (IndexError, UnicodeEncodeError) as e:
+                        print i['snippet']['title'], str(e)
+                        flag = True
+                        pass
+                    except KeyError:
+                        print i['snippet']['title']
+                        if faces.get('error').get('code') == "RateLimitExceeded":
+                            print "RateLimitExceeded"
+                            time.sleep(10)
+                            continue
+                        flag = True
---
+++ output.py
@@ -0,0 +1,53 @@
+import csv
+
+if __name__ == "__main__":
+    with open("all_needledrop_ratings.csv", "r") as f1, open("results3.csv", "r") as f2:
+
+        ratings = csv.reader(f1, delimiter=",")
+        next(ratings, None)
+        next(ratings, None)
+        next(ratings, None)
+        ratings_map = dict()
+
+        for rating in ratings:
+            artist, album, score, newline = rating
+            artist = artist.strip()
+            album = album.strip()
+            score = score.strip()
+
+            if artist not in ratings_map:
+                ratings_map[artist] = dict()
+
+            ratings_map[artist][album] = score
+
+        emotions = csv.reader(f2, delimiter=",")
+        next(emotions, None)
+
+        count = 0
+
+        with open("final.csv","wb") as output:
+            writer = csv.writer(output, delimiter=',')
+            writer.writerow(
+                ["title", "image_url", "sadness", "neutral", "contempt", "disgust", "anger", "surprise", "fear",
+                 "happiness","score"])
+            for emotion in emotions:
+                count += 1
+                print emotion
+                try:
+                    artist, album = emotion[0].split(" - ")
+                except ValueError:
+                    print "ValueError"
+                    artist, album = emotion[0].split("- ")
+
+                album = album.replace("ALBUM REVIEWS", "").replace("ALBUM REVIEW", "").strip()
+                album = " ".join(album.split())
+
+                output_line = emotion
+                try:
+                    output_line.append(ratings_map[artist][album])
+                    writer.writerow(output_line)
+                except KeyError as e:
+                    print count
+                    print e
+                    print ratings_map.get(artist, "")
+                    raw_input()
